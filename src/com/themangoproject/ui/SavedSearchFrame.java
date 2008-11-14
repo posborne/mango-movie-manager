0d699a7f53e9bcac0f3f351b57f880e23ecd9cf3
ronkyl <ronkyl@e0e46c49-be69-4f5a-ad62-21024a331aea>
2008-11-14 23:33:35 +0000

    SavedSearch disposes on close instead of exiting the whole program.
    git-svn-id: http://svn.assembla.com/svn/mangoproject/trunk@20 e0e46c49-be69-4f5a-ad62-21024a331aea

--- src/com/themangoproject/ui/SavedSearchFrame.java
+++ src/com/themangoproject/ui/SavedSearchFrame.java
@@ -31,7 +31,7 @@ public class SavedSearchFrame extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Saved Search:");
 
         jLabel1.setText("Name");
