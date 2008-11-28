1a257da0d727dac15d282b2582032ebd9eebf3a9
posborne <posborne@e0e46c49-be69-4f5a-ad62-21024a331aea>
2008-11-28 23:38:31 +0000

    Switched up the Navigator Panel stuff so that it more accurately represents what we are looking for in the end.  As of yet, none of the actions are implemented.
    git-svn-id: http://svn.assembla.com/svn/mangoproject/trunk@53 e0e46c49-be69-4f5a-ad62-21024a331aea

--- src/com/themangoproject/ui/NavigatorRootNode.java
+++ src/com/themangoproject/ui/NavigatorRootNode.java
@@ -8,32 +8,27 @@ import javax.swing.tree.DefaultMutableTreeNode;
  */
 class NavigatorRootNode extends DefaultMutableTreeNode {
 
-    public NavigatorRootNode() {
-        super("Invisible Root Node", true);
-        DefaultMutableTreeNode actions = new DefaultMutableTreeNode("Basic Actions?", true);
-        actions.add(new DefaultMutableTreeNode("One"));
-        actions.add(new DefaultMutableTreeNode("Two"));
-        actions.add(new DefaultMutableTreeNode("Three"));
-        actions.add(new DefaultMutableTreeNode("Four"));
+	private static final long serialVersionUID = 6737021383031998889L;
 
-        DefaultMutableTreeNode savedSearches = new DefaultMutableTreeNode("Saved Searches", true);
-        savedSearches.add(new DefaultMutableTreeNode("One"));
-        savedSearches.add(new DefaultMutableTreeNode("Two"));
-        savedSearches.add(new DefaultMutableTreeNode("Three"));
-        savedSearches.add(new DefaultMutableTreeNode("Four"));
+	public NavigatorRootNode() {
+        super("Invisible Root Node", true);
+        DefaultMutableTreeNode actions = new DefaultMutableTreeNode("Library", true);
+        actions.add(new DefaultMutableTreeNode("All Movies"));
+        actions.add(new DefaultMutableTreeNode("My Movies"));
+        actions.add(new DefaultMutableTreeNode("Friend's Movies"));
+        actions.add(new DefaultMutableTreeNode("Borrowed Movies"));
+        actions.add(new DefaultMutableTreeNode("Loaned Movies"));
+        actions.add(new DefaultMutableTreeNode("Wish List"));
+        
+        DefaultMutableTreeNode sets = new DefaultMutableTreeNode("Sets", true);
+        sets.add(new DefaultMutableTreeNode("Create New Set"));
         
         DefaultMutableTreeNode lists = new DefaultMutableTreeNode("Lists", true);
-        lists.add(new DefaultMutableTreeNode("One"));
-        lists.add(new DefaultMutableTreeNode("Two"));
-        lists.add(new DefaultMutableTreeNode("Three"));
-        lists.add(new DefaultMutableTreeNode("Four"));
-
-        DefaultMutableTreeNode sets = new DefaultMutableTreeNode("Sets", true);
-        sets.add(new DefaultMutableTreeNode("One"));
-        sets.add(new DefaultMutableTreeNode("Two"));
-        sets.add(new DefaultMutableTreeNode("Three"));
-        sets.add(new DefaultMutableTreeNode("Four"));
-
+        lists.add(new DefaultMutableTreeNode("Create New List"));
+        
+        DefaultMutableTreeNode savedSearches = new DefaultMutableTreeNode("Saved Searches", true);
+        savedSearches.add(new DefaultMutableTreeNode("Create New Saved Search"));
+        
         this.add(actions);
         this.add(savedSearches);
         this.add(lists);
