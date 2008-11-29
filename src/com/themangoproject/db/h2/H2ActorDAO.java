4ff53907ecc03161a508bf7d6cee53c3fa0cf508
posborne <posborne@e0e46c49-be69-4f5a-ad62-21024a331aea>
2008-11-29 01:29:17 +0000

    Added javadoc documentation to H2ActorDAO.
    git-svn-id: http://svn.assembla.com/svn/mangoproject/trunk@57 e0e46c49-be69-4f5a-ad62-21024a331aea

--- src/com/themangoproject/db/h2/H2ActorDAO.java
+++ src/com/themangoproject/db/h2/H2ActorDAO.java
@@ -5,124 +5,174 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The <code>H2ActorDAO</code> is an <code>ActorDAO</code> that provides
+ * methods for communicating with the H2 database system, specifically with
+ * regards to queries and updates that affect the <code>Actor</code> business
+ * object.
+ * 
+ * The DAO is a singleton and obtains and manages its own database connection.
+ * 
+ * @author Paul Osborne
+ * @version November 28, 2008
+ */
 public class H2ActorDAO implements ActorDAO {
 
-    private static H2ActorDAO instance;
-    private Connection conn;
+	/** Singleton instance of the H2ActorDAO */
+	private static H2ActorDAO instance;
 
-    private PreparedStatement allActorsPS;
-    private static final String allActorsQuery =
-            "SELECT (id, first_name, last_name) FROM actor";
+	/** Database connection for the DAO */
+	private Connection conn;
 
-    private PreparedStatement rolesForActorPS;
-    private static final String rolesForActorQuery =
-            "SELECT title, role, character" +
-            " FROM movie, acting_roles" +
-            " WHERE movie_id=? AND actor_id=?";
+	/** Prepared statement for retrieving ids of all actors */
+	private PreparedStatement allActorsPS;
+	/** Retrieve ids and names of all actors */
+	private static final String allActorsQuery = "SELECT (id, first_name, last_name) FROM actor";
 
-    private PreparedStatement addActorPS;
-    private static final String addActorQuery =
-            "INSERT INTO actor (first_name, last_name)" +
-            " VALUES(?, ?)";
+	/** Retrieve all roles for an actor */
+	private PreparedStatement rolesForActorPS;
+	private static final String rolesForActorQuery = "SELECT title, role, character"
+			+ " FROM movie, acting_roles"
+			+ " WHERE movie_id=? AND actor_id=?";
 
-    private PreparedStatement updateActorPS;
-    private static final String updateActorQuery =
-            "UPDATE actor" +
-            " SET first_name=?, last_name=?" +
-            " WHERE id=?";
+	/** Add an actor to the database (no roles) */
+	private PreparedStatement addActorPS;
+	private static final String addActorQuery = "INSERT INTO actor (first_name, last_name)"
+			+ " VALUES(?, ?)";
 
-    private PreparedStatement populatePersonPS;
-    private static final String populatePersonQuery =
-            "SELECT first_name, last_name" +
-            " FROM actor" +
-            " WHERE id=?";
+	/** Update an actor in the database */
+	private PreparedStatement updateActorPS;
+	private static final String updateActorQuery = "UPDATE actor"
+			+ " SET first_name=?, last_name=?" + " WHERE id=?";
 
-    private H2ActorDAO() {
-        conn = H2Util.getInstance().getConnection();
-        try {
-            allActorsPS = conn.prepareStatement(allActorsQuery);
-            updateActorPS = conn.prepareStatement(updateActorQuery);
-            rolesForActorPS = conn.prepareStatement(rolesForActorQuery);
-            addActorPS = conn.prepareStatement(addActorQuery);
-            updateActorPS = conn.prepareStatement(updateActorQuery);
-            populatePersonPS = conn.prepareStatement(populatePersonQuery);
-        } catch (SQLException ex) {
-            // TODO; decide what to do here
-            ex.printStackTrace();
-        }
+	/** Retrive an actors information */
+	private PreparedStatement populatePersonPS;
+	private static final String populatePersonQuery = "SELECT first_name, last_name"
+			+ " FROM actor" + " WHERE id=?";
 
-    }
+	/**
+	 * The DAOs private constructor prepares the various, frequently used
+	 * queries as Prepared Statements and otherwise initializes the state of the
+	 * data access object.
+	 */
+	private H2ActorDAO() {
+		conn = H2Util.getInstance().getConnection();
+		try {
+			allActorsPS = conn.prepareStatement(allActorsQuery);
+			updateActorPS = conn.prepareStatement(updateActorQuery);
+			rolesForActorPS = conn.prepareStatement(rolesForActorQuery);
+			addActorPS = conn.prepareStatement(addActorQuery);
+			updateActorPS = conn.prepareStatement(updateActorQuery);
+			populatePersonPS = conn
+					.prepareStatement(populatePersonQuery);
+		} catch (SQLException ex) {
+			// TODO; decide what to do here
+			ex.printStackTrace();
+		}
 
-    public static H2ActorDAO getInstance() {
-        if (instance == null) {
-            instance = new H2ActorDAO();
-        }
-        return instance;
-    }
+	}
 
-    public List<Actor> getAllActors() {
-        ArrayList<Actor> actors = new ArrayList<Actor>();
-        try {
-            ResultSet results = allActorsPS.executeQuery();
-            while (results.next()) {
-                DBActor actor = new DBActor();
-                actor.setId(results.getInt("id"));
-                actor.setFirstName(results.getString("first_name"));
-                actor.setLastName(results.getString("last_name"));
-                actors.add(actor);
-            }
-        } catch (SQLException ex) {
-            // TODO: decide what to do here
-            ex.printStackTrace();
-        }
-        return actors;
-    }
+	/**
+	 * @return A singleton instance of the DAO.
+	 */
+	public static H2ActorDAO getInstance() {
+		if (instance == null) {
+			instance = new H2ActorDAO();
+		}
+		return instance;
+	}
 
-    public List<Role> getRolesForActor(Actor a) {
-        if (!(a instanceof DBActor)) {
-            throw new ClassCastException();
-        }
-        DBActor actor = (DBActor) a;
+	/**
+	 * @return A list of all the actors in the database.
+	 */
+	public List<Actor> getAllActors() {
+		ArrayList<Actor> actors = new ArrayList<Actor>();
+		try {
+			ResultSet results = allActorsPS.executeQuery();
+			while (results.next()) {
+				DBActor actor = new DBActor();
+				actor.setId(results.getInt("id"));
+				actor.setFirstName(results.getString("first_name"));
+				actor.setLastName(results.getString("last_name"));
+				actors.add(actor);
+			}
+		} catch (SQLException ex) {
+			// TODO: decide what to do here
+			ex.printStackTrace();
+		}
+		return actors;
+	}
 
-        ArrayList<Role> roles = new ArrayList<Role>();
-        try {
-            ResultSet results = rolesForActorPS.executeQuery();
-            while (results.next()) {
-                DBRole role = new DBRole();
-                //role.setActorId(id);
-                // TODO: finish this
-            }
-        } catch (SQLException ex) {
-            // TODO: decide what to do here
-            ex.printStackTrace();
-        }
-        return roles;
-    }
+	/**
+	 * Retrieve a list of all the roles for a specified actor.
+	 * 
+	 * @param a
+	 *            The actor for which the roles are desired (TODO: this sentence
+	 *            could be read the wrong way).
+	 * @return a list of all the roles for the specified actor.
+	 * @throws ClassCastException
+	 *             if the Actor passed in is not a DBActor.
+	 */
+	public List<Role> getRolesForActor(Actor a) {
+		if (!(a instanceof DBActor)) {
+			throw new ClassCastException();
+		}
+		DBActor actor = (DBActor) a;
 
-    public void addActor(Actor actor) {
-        try {
-            addActorPS.setString(0, actor.getFirstName());
-            addActorPS.setString(1, actor.getLastName());
-            addActorPS.execute();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-    }
+		ArrayList<Role> roles = new ArrayList<Role>();
+		try {
+			ResultSet results = rolesForActorPS.executeQuery();
+			while (results.next()) {
+				DBRole role = new DBRole();
+				// role.setActorId(id);
+				// TODO: finish this
+			}
+		} catch (SQLException ex) {
+			// TODO: decide what to do here
+			ex.printStackTrace();
+		}
+		return roles;
+	}
 
-    public void updateActor(Actor a) {
-        if (!(a instanceof DBActor)) {
-            throw new ClassCastException();
-        }
-        DBActor actor = (DBActor) a;
+	/**
+	 * Add the specified actor to the database.
+	 * 
+	 * @param actor
+	 *            The actor that should be added to the database.
+	 */
+	public void addActor(Actor actor) {
+		try {
+			addActorPS.setString(0, actor.getFirstName());
+			addActorPS.setString(1, actor.getLastName());
+			addActorPS.execute();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
 
-        try {
-            updateActorPS.setString(0, actor.getFirstName());
-            updateActorPS.setString(1, actor.getLastName());
-            updateActorPS.setInt(3, actor.getId());
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-    }
+	/**
+	 * Update the specified database actor in the database with the information
+	 * contained in the object.
+	 * 
+	 * @param a
+	 *            The actor object containing the information that should be
+	 *            added to the database.
+	 * @throws ClassCastException
+	 *             if the Actor passed in is not a DBActor.
+	 */
+	public void updateActor(Actor a) {
+		if (!(a instanceof DBActor)) {
+			throw new ClassCastException();
+		}
+		DBActor actor = (DBActor) a;
+
+		try {
+			updateActorPS.setString(0, actor.getFirstName());
+			updateActorPS.setString(1, actor.getLastName());
+			updateActorPS.setInt(3, actor.getId());
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
 
 }
-
