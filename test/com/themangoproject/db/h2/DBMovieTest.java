0ab5989e4da26b26cb3294c63687280b91104bbd
varzac <varzac@e0e46c49-be69-4f5a-ad62-21024a331aea>
2008-12-02 15:15:25 +0000

    Stubs for a DBMovieTest junit test.  YEAH commit #100.
    git-svn-id: http://svn.assembla.com/svn/mangoproject/trunk@100 e0e46c49-be69-4f5a-ad62-21024a331aea

---
+++ test/com/themangoproject/db/h2/DBMovieTest.java
@@ -0,0 +1,320 @@
+/**
+ * 
+ */
+package com.themangoproject.db.h2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Zachary Varberg
+ *
+ */
+public class DBMovieTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#DBMovie()}.
+     */
+    @Test
+    public void testDBMovie() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#getId()}.
+     */
+    @Test
+    public void testGetId() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#setDirector(java.lang.String)}.
+     */
+    @Test
+    public void testSetDirector() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#getDirector()}.
+     */
+    @Test
+    public void testGetDirector() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#setTitle(java.lang.String)}.
+     */
+    @Test
+    public void testSetTitle() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#getTitle()}.
+     */
+    @Test
+    public void testGetTitle() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#setRating(java.lang.String)}.
+     */
+    @Test
+    public void testSetRating() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#getRating()}.
+     */
+    @Test
+    public void testGetRating() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#setRuntime(int)}.
+     */
+    @Test
+    public void testSetRuntime() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#getRuntime()}.
+     */
+    @Test
+    public void testGetRuntime() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#setMangoRating(int)}.
+     */
+    @Test
+    public void testSetMangoRating() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#getMangoRating()}.
+     */
+    @Test
+    public void testGetMangoRating() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#getActors()}.
+     */
+    @Test
+    public void testGetActors() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#addActor(com.themangoproject.model.Actor)}.
+     */
+    @Test
+    public void testAddActor() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#removeActor(com.themangoproject.model.Actor)}.
+     */
+    @Test
+    public void testRemoveActor() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#getYear()}.
+     */
+    @Test
+    public void testGetYear() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#setYear(int)}.
+     */
+    @Test
+    public void testSetYear() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#getASIN()}.
+     */
+    @Test
+    public void testGetASIN() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#setASIN(java.lang.String)}.
+     */
+    @Test
+    public void testSetASIN() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#getPurchaseDate()}.
+     */
+    @Test
+    public void testGetPurchaseDate() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#setPurchaseDate(java.util.Date)}.
+     */
+    @Test
+    public void testSetPurchaseDate() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#getCustomDescription()}.
+     */
+    @Test
+    public void testGetCustomDescription() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#setCustomDescription(java.lang.String)}.
+     */
+    @Test
+    public void testSetCustomDescription() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#getType()}.
+     */
+    @Test
+    public void testGetType() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#setType(java.lang.String)}.
+     */
+    @Test
+    public void testSetType() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#getOwner()}.
+     */
+    @Test
+    public void testGetOwner() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#setOwner(com.themangoproject.model.Person)}.
+     */
+    @Test
+    public void testSetOwner() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#getBorrower()}.
+     */
+    @Test
+    public void testGetBorrower() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#setBorrower(com.themangoproject.model.Person)}.
+     */
+    @Test
+    public void testSetBorrower() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#getGenres()}.
+     */
+    @Test
+    public void testGetGenres() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#addGenre(java.lang.String)}.
+     */
+    @Test
+    public void testAddGenre() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#removeGenre(java.lang.String)}.
+     */
+    @Test
+    public void testRemoveGenre() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#getCondition()}.
+     */
+    @Test
+    public void testGetCondition() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#setCondition(java.lang.String)}.
+     */
+    @Test
+    public void testSetCondition() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#getOwnerId()}.
+     */
+    @Test
+    public void testGetOwnerId() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.themangoproject.db.h2.DBMovie#getBorrowerId()}.
+     */
+    @Test
+    public void testGetBorrowerId() {
+        fail("Not yet implemented");
+    }
+
+}
