ce0280da0508c538dc54bee59a30fa05c1478881
ronkyl <ronkyl@e0e46c49-be69-4f5a-ad62-21024a331aea>
2008-12-04 07:11:46 +0000

    Created table models.  None are implemented.  I just have an idea in my head of how they should be used.
    git-svn-id: http://svn.assembla.com/svn/mangoproject/trunk@111 e0e46c49-be69-4f5a-ad62-21024a331aea

---
+++ src/com/themangoproject/ui/model/EditableMovieTableModel.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.themangoproject.ui.model;
+
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author kronning
+ */
+public class EditableMovieTableModel extends AbstractTableModel{
+
+    public int getRowCount() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public int getColumnCount() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
