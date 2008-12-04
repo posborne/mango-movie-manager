29c68d4313dcd049a70dfd30edacc8b5b0c4d4f9
ronkyl <ronkyl@e0e46c49-be69-4f5a-ad62-21024a331aea>
2008-12-04 17:55:32 +0000

    Added combo box models.  This should be failry easy to implement.
    git-svn-id: http://svn.assembla.com/svn/mangoproject/trunk@117 e0e46c49-be69-4f5a-ad62-21024a331aea

---
+++ src/com/themangoproject/ui/model/ActorComboBoxModel.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.themangoproject.ui.model;
+
+/**
+ *
+ * @author kronning
+ */
+public class ActorComboBoxModel {
+
+}
