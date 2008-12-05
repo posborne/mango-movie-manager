0eb4f64f02e876fc81084fce337106651d05586d
posborne <posborne@e0e46c49-be69-4f5a-ad62-21024a331aea>
2008-12-05 01:53:24 +0000

    Put in some templating/setup for ActorTableModel and added a toString() to DBActor.
    git-svn-id: http://svn.assembla.com/svn/mangoproject/trunk@129 e0e46c49-be69-4f5a-ad62-21024a331aea

--- src/com/themangoproject/db/h2/DBActor.java
+++ src/com/themangoproject/db/h2/DBActor.java
@@ -4,153 +4,157 @@ import com.themangoproject.model.*;
 import java.util.List;
 
 /**
- * This is a concrete implementation of the Actor interface. This is a
- * DataBase actor so it has limited knowledge of some DB ideas. This
- * class is meant to represent an actor and contains information
- * important to an actor such as name and roles and movies the actor
- * participated in.
+ * This is a concrete implementation of the Actor interface. This is a DataBase
+ * actor so it has limited knowledge of some DB ideas. This class is meant to
+ * represent an actor and contains information important to an actor such as
+ * name and roles and movies the actor participated in.
  * 
  * @author Zachary Varberg
  */
 public class DBActor implements Actor {
 
-    private int id;
-    private String firstName;
-    private String lastName;
-    private ActorDAO actorDAO;
-    private DBActorState state;
+	private int id;
+	private String firstName;
+	private String lastName;
+	private ActorDAO actorDAO;
+	private DBActorState state;
 
-    public DBActor() {
-        this(-1, null, null);
-    }
+	public DBActor() {
+		this(-1, null, null);
+	}
 
-    public DBActor(int id) {
-        this(id, null, null);
-    }
+	public DBActor(int id) {
+		this(id, null, null);
+	}
 
-    public DBActor(int id, String firstName, String lastName) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        H2DAOFactory fact = new H2DAOFactory();
-        this.state = new NotfilledActorState();
-        this.actorDAO = fact.getActorDAO();
-    }
+	public DBActor(int id, String firstName, String lastName) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		H2DAOFactory fact = new H2DAOFactory();
+		this.state = new NotfilledActorState();
+		this.actorDAO = fact.getActorDAO();
+	}
 
-    /**
-     * This will return the db ID of this Actor
-     * 
-     * @return the db ID of this actor.
-     */
-    int getId() {
-        return id;
-    }
+	/**
+	 * This will return the db ID of this Actor
+	 * 
+	 * @return the db ID of this actor.
+	 */
+	int getId() {
+		return id;
+	}
 
-    /**
-     * This will return a list of all the movies this actor has been a
-     * part of.
-     * 
-     * @return a list of all the movies this actor has been a part of.
-     */
-    public List<Movie> getMovies() {
-        return actorDAO.getMoviesForActor(this);
-    }
+	/**
+	 * This will return a list of all the movies this actor has been a part of.
+	 * 
+	 * @return a list of all the movies this actor has been a part of.
+	 */
+	public List<Movie> getMovies() {
+		return actorDAO.getMoviesForActor(this);
+	}
 
-    /**
-     * This will return a list of all the roles this movie has
-     * participated in. A role contains information such as the movie,
-     * the actors role (e.g. lead actor, or supporting actress), and
-     * the character played by the actor in that movie.
-     * 
-     * @return the list of all the roles this actor has been a part
-     *         of.
-     */
-    public List<Role> getRoles() {
-        return this.actorDAO.getRolesForActor(this);
-    }
+	/**
+	 * This will return a list of all the roles this movie has participated in.
+	 * A role contains information such as the movie, the actors role (e.g. lead
+	 * actor, or supporting actress), and the character played by the actor in
+	 * that movie.
+	 * 
+	 * @return the list of all the roles this actor has been a part of.
+	 */
+	public List<Role> getRoles() {
+		return this.actorDAO.getRolesForActor(this);
+	}
 
-    /**
-     * This will return the first name of this actor
-     * 
-     * @return the first name of this actor
-     */
-    public String getFirstName() {
-        return this.state.getFirstName();
-    }
+	/**
+	 * This will return the first name of this actor
+	 * 
+	 * @return the first name of this actor
+	 */
+	public String getFirstName() {
+		return this.state.getFirstName();
+	}
 
-    /**
-     * This will return the last name of this actor
-     * 
-     * @return the last name of this actor
-     */
-    public String getLastName() {
-        return this.state.getLastName();
-    }
+	/**
+	 * This will return the last name of this actor
+	 * 
+	 * @return the last name of this actor
+	 */
+	public String getLastName() {
+		return this.state.getLastName();
+	}
 
-    /**
-     * This will set the first name of this actor.
-     * 
-     * @param firstName
-     *            the first name of this actor.
-     */
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+	/**
+	 * This will set the first name of this actor.
+	 * 
+	 * @param firstName
+	 *            the first name of this actor.
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-    /**
-     * This will set the last name of this actor.
-     * 
-     * @param lastName
-     *            the last name of this actor.
-     */
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+	/**
+	 * This will set the last name of this actor.
+	 * 
+	 * @param lastName
+	 *            the last name of this actor.
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-    /**
-     * This will set the ID of this actor.
-     * 
-     * @param id
-     *            the ID of this actor.
-     */
-    void setId(int id) {
-        this.id = id;
-    }
+	/**
+	 * @return The full name of the actor.
+	 */
+	public String toString() {
+		return this.getFirstName() + " " + this.getLastName();
+	}
 
-    private class NotfilledActorState implements DBActorState {
+	/**
+	 * This will set the ID of this actor.
+	 * 
+	 * @param id
+	 *            the ID of this actor.
+	 */
+	void setId(int id) {
+		this.id = id;
+	}
 
-        @Override
-        public String getFirstName() {
-            if (id != -1) {
-                DBActor.this.actorDAO.populateActor(DBActor.this);
-                DBActor.this.state = new UpdatedActorState();
-            }
-            return DBActor.this.firstName;
-        }
+	private class NotfilledActorState implements DBActorState {
 
-        @Override
-        public String getLastName() {
-            if (id != -1) {
-                DBActor.this.actorDAO.populateActor(DBActor.this);
-                DBActor.this.state = new UpdatedActorState();
-            }
-            return DBActor.this.lastName;
-        }
+		@Override
+		public String getFirstName() {
+			if (id != -1) {
+				DBActor.this.actorDAO.populateActor(DBActor.this);
+				DBActor.this.state = new UpdatedActorState();
+			}
+			return DBActor.this.firstName;
+		}
 
-    }
+		@Override
+		public String getLastName() {
+			if (id != -1) {
+				DBActor.this.actorDAO.populateActor(DBActor.this);
+				DBActor.this.state = new UpdatedActorState();
+			}
+			return DBActor.this.lastName;
+		}
 
-    private class UpdatedActorState implements DBActorState {
+	}
 
-        @Override
-        public String getFirstName() {
-            return DBActor.this.firstName;
-        }
+	private class UpdatedActorState implements DBActorState {
 
-        @Override
-        public String getLastName() {
-            return DBActor.this.lastName;
-        }
+		@Override
+		public String getFirstName() {
+			return DBActor.this.firstName;
+		}
 
-    }
+		@Override
+		public String getLastName() {
+			return DBActor.this.lastName;
+		}
+
+	}
 
 }
--- src/com/themangoproject/ui/model/ActorTableModel.java
+++ src/com/themangoproject/ui/model/ActorTableModel.java
@@ -1,14 +1,58 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.themangoproject.ui.model;
 
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+import com.themangoproject.model.Person;
+
 /**
- *
- * @author kronning
+ * Table model encapsulating a view of the People contained in the Mango DB
+ * backend.
+ * 
+ * @author Paul Osborne
  */
-public class ActorTableModel {
+public class ActorTableModel extends AbstractTableModel {
+
+	/** Generated serial version UID */
+	private static final long serialVersionUID = -392463901990643277L;
+
+	/** List of people in the storage backend */
+	private List<Person> persons;
+
+	/**
+	 * The number of columns is the number of persons in the table.
+	 * 
+	 * @return The number of people who fit the current model criterium
+	 */
+	@Override
+	public int getColumnCount() {
+		return 1; // TODO: implement getColumnCount
+	}
+
+	/**
+	 * Get the number of rows in the model.
+	 * 
+	 * @return the number of rows in the model.
+	 */
+	@Override
+	public int getRowCount() {
+		return persons.size();
+	}
+
+	/**
+	 * Get the value of the object at the specified row and column position in
+	 * the table model.
+	 * 
+	 * @param rowIndex
+	 *            the row index
+	 * @param columnIndex
+	 *            the column index
+	 * @return The value of the Object corresponding to the specified row and
+	 *         column index.
+	 */
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
