160d7bb4f895f657be2ddb2061889fe2cd5b6751
posborne <posborne@e0e46c49-be69-4f5a-ad62-21024a331aea>
2008-12-05 01:28:06 +0000

    Put in some base implementation for the Actor and Person ComboBoxModels.
    git-svn-id: http://svn.assembla.com/svn/mangoproject/trunk@128 e0e46c49-be69-4f5a-ad62-21024a331aea

--- src/com/themangoproject/ui/model/ActorComboBoxModel.java
+++ src/com/themangoproject/ui/model/ActorComboBoxModel.java
@@ -1,14 +1,47 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.themangoproject.ui.model;
 
+import java.util.List;
+import javax.swing.DefaultComboBoxModel;
+import com.themangoproject.model.Actor;
+import com.themangoproject.model.MangoController;
+
 /**
- *
- * @author kronning
+ * This combo box model gives encapsulates the list of all actors that exist.
+ * The list should be automatically updated when the a method to change the
+ * database occurs, and the client code can retrieve an Actor object from the
+ * combo box model from its index.
+ * 
+ * @author Paul Osborne
  */
-public class ActorComboBoxModel {
+public class ActorComboBoxModel extends DefaultComboBoxModel {
+
+	/** generated serial version uid */
+	private static final long serialVersionUID = 1841056682059744938L;
+
+	/** List of all actors */
+	private List<Actor> actors;
+
+	/**
+	 * Construct an ActorComboBoxModel.
+	 */
+	public ActorComboBoxModel() {
+		actors = MangoController.getInstance().getAllActors();
+	}
+
+	/**
+	 * @return the number of actors in the model
+	 */
+	@Override
+	public int getSize() {
+		return actors.size();
+	}
+
+	/**
+	 * @return the actor at the specified index in the model.
+	 */
+	@Override
+	public Object getElementAt(int index) {
+		return actors.get(index);
+	}
 
 }
--- src/com/themangoproject/ui/model/PersonComboBoxModel.java
+++ src/com/themangoproject/ui/model/PersonComboBoxModel.java
@@ -1,14 +1,45 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.themangoproject.ui.model;
 
+import java.util.List;
+
+import javax.swing.DefaultComboBoxModel;
+import com.themangoproject.model.*;
+
 /**
- *
- * @author kronning
+ * Combo box model that encapsulates a model of all the people contained in the
+ * database.
+ * 
+ * @author Paul Osborne
  */
-public class PersonComboBoxModel {
+public class PersonComboBoxModel extends DefaultComboBoxModel {
+
+	/** Generated serial version uid */
+	private static final long serialVersionUID = -164842922152566685L;
+
+	/** List of persons in the database */
+	private List<Person> persons;
+
+	/**
+	 * Instatiate an instance of a PersonComboBoxModel.
+	 */
+	public PersonComboBoxModel() {
+		this.persons = MangoController.getInstance().getAllPersons();
+	}
+
+	/**
+	 * @return the number of <code>People</code> in the model.
+	 */
+	@Override
+	public int getSize() {
+		return persons.size();
+	}
+
+	/**
+	 * @return the <code>Person</code> at the specified index in the model.
+	 */
+	@Override
+	public Object getElementAt(int index) {
+		return persons.get(index);
+	}
 
 }
