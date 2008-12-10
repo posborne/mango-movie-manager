6c551d8583ca3324571b70da6fd233954ba51ebe
posborne <posborne@e0e46c49-be69-4f5a-ad62-21024a331aea>
2008-12-10 08:20:09 +0000

    Added javadocs to H2SetsDAO.  It was looking a bit naked.
    git-svn-id: http://svn.assembla.com/svn/mangoproject/trunk@191 e0e46c49-be69-4f5a-ad62-21024a331aea

--- src/com/themangoproject/db/h2/H2SetsDAO.java
+++ src/com/themangoproject/db/h2/H2SetsDAO.java
@@ -13,43 +13,57 @@ import com.themangoproject.model.Movie;
 import com.themangoproject.model.SetsDAO;
 
 /**
+ * The H2SetsDAO is an implementation of the SetsDAO specific to the H2
+ * database. These methods are responsible for doing different database layer
+ * interactions involving sets.
+ * 
  * @author Paul Osborne
  */
 public class H2SetsDAO implements SetsDAO {
 
+	/** Singleton instance */
 	private static H2SetsDAO instance;
+	/** Database connection that will be used */
 	private Connection conn;
-	
+
+	/** Retrieve labels of all sets */
 	private PreparedStatement allSetsPS;
-	private static final String allSetsSQL =
-		"SELECT label FROM sets";
-	
+	private static final String allSetsSQL = "SELECT label FROM sets";
+
+	/** Retrieve ids of all movies in a set */
 	private PreparedStatement moviesInSetPS;
-	private static final String moviesInSetSQL =
-		"SELECT movie_id FROM set_contents WHERE " +
-		"    set_id IN (SELECT id FROM sets WHERE label=?)";
-	
+	private static final String moviesInSetSQL = "SELECT movie_id FROM set_contents WHERE "
+			+ "    set_id IN (SELECT id FROM sets WHERE label=?)";
+
+	/** Remove a set from the set_contents table */
 	private PreparedStatement removeSetFromSetContentsPS;
-	private static final String removeSetSQL =
-		"DELETE FROM set_contents WHERE set_id IN (SELECT id FROM sets WHERE label=?)";
-	
+	private static final String removeSetSQL = "DELETE FROM set_contents WHERE set_id IN (SELECT id FROM sets WHERE label=?)";
+
+	/** Remove a set from the sets table */
 	private PreparedStatement removeSetFromSetsPS;
-	private static final String removeSetFromSetsSQL =
-		"DELETE FROM sets WHERE label=?";
-	
+	private static final String removeSetFromSetsSQL = "DELETE FROM sets WHERE label=?";
+
+	/** Remove a specific movie from a list */
 	private PreparedStatement removeMovieFromSetPS;
-	private static final String removeMovieFromSetSQL =
-		"DELETE FROM set_contents WHERE set_id IN (SELECT id FROM sets WHERE label=?) AND movie_id=?";
-	
+	private static final String removeMovieFromSetSQL = "DELETE FROM set_contents WHERE set_id IN (SELECT id FROM sets WHERE label=?) AND movie_id=?";
+
+	/** Add a movie to a set */
 	private PreparedStatement addMovieToSetPS;
-	private static final String addMovieToSetSQL =
-		"INSERT INTO set_contents (set_id, movie_id) " +
-		"	VALUES(?, ?)";
-	
+	private static final String addMovieToSetSQL = "INSERT INTO set_contents (set_id, movie_id) "
+			+ "	VALUES(?, ?)";
+
+	/** Retrieve the set_id for a label */
 	private PreparedStatement setIdForLabelPS;
-	private static final String setIdForLabelSQL =
-		"SELECT id FROM sets WHERE label=?";
-	
+	private static final String setIdForLabelSQL = "SELECT id FROM sets WHERE label=?";
+
+	/** Add a new set to the list of sets */
+	private PreparedStatement addSetPS;
+	private static final String addSetSQL = "INSERT INTO sets(label) VALUES(?)";
+
+	/**
+	 * Private singleton constructor initializes different Prepared Statements
+	 * and other state for the singleton.
+	 */
 	private H2SetsDAO() {
 		conn = H2Util.getInstance().getConnection();
 		setsChangeListeners = new ArrayList<ChangeListener>();
@@ -66,7 +80,12 @@ public class H2SetsDAO implements SetsDAO {
 			ex.printStackTrace();
 		}
 	}
-	
+
+	/**
+	 * Get the singleton instance of the DAO.
+	 * 
+	 * @return the singleton instance of the H2SetsDAO.
+	 */
 	public static H2SetsDAO getInstance() {
 		if (instance == null) {
 			instance = new H2SetsDAO();
@@ -74,10 +93,12 @@ public class H2SetsDAO implements SetsDAO {
 		return instance;
 	}
 
-	private PreparedStatement addSetPS;
-	private static final String addSetSQL =
-		"INSERT INTO sets(label) VALUES(?)";
-	
+	/**
+	 * Add a new set with the specified label to the database
+	 * 
+	 * @param label
+	 *            The label for the new set.
+	 */
 	public void addSet(String label) {
 		try {
 			addSetPS.setString(1, label);
@@ -87,7 +108,12 @@ public class H2SetsDAO implements SetsDAO {
 		}
 		fireSetsChangedEvent();
 	}
-	
+
+	/**
+	 * Get a list of all set labels in the database.
+	 * 
+	 * @return a list of all sets in the database.
+	 */
 	@Override
 	public List<String> getAllSets() {
 		ArrayList<String> sets = new ArrayList<String>();
@@ -102,6 +128,13 @@ public class H2SetsDAO implements SetsDAO {
 		return sets;
 	}
 
+	/**
+	 * Retrieve a list of all movies in the specified set.
+	 * 
+	 * @param label
+	 *            The label of the set we are interested in.
+	 * @return a list of all the movies in a set.
+	 */
 	@Override
 	public List<Movie> getMoviesInSet(String label) {
 		ArrayList<Movie> movies = new ArrayList<Movie>();
@@ -119,6 +152,12 @@ public class H2SetsDAO implements SetsDAO {
 		return movies;
 	}
 
+	/**
+	 * Remove the set with the specified label from the database.
+	 * 
+	 * @param label
+	 *            The label of the set that should be removed.
+	 */
 	@Override
 	public void removeSet(String label) {
 		try {
@@ -135,17 +174,33 @@ public class H2SetsDAO implements SetsDAO {
 		fireSetsChangedEvent();
 	}
 
+	/**
+	 * Remove a specific movie from a set
+	 * 
+	 * @param m
+	 *            The movie that should be removed.
+	 * @param label
+	 *            The set from which the movie should be removed.
+	 */
 	@Override
 	public void removeMovieFromSet(String label, Movie m) {
 		try {
 			removeMovieFromSetPS.setString(1, label);
-			removeMovieFromSetPS.setInt(2,((DBMovie)m).getId());
+			removeMovieFromSetPS.setInt(2, ((DBMovie) m).getId());
 			removeMovieFromSetPS.executeUpdate();
 		} catch (SQLException ex) {
 			ex.printStackTrace();
 		}
 	}
 
+	/**
+	 * Add the specified movie to the set with the specified label.
+	 * 
+	 * @param label
+	 *            The label of the set we should add to.
+	 * @param m
+	 *            The movie that should be added.
+	 */
 	@Override
 	public void addMovieToSet(String label, Movie m) {
 		try {
@@ -155,29 +210,42 @@ public class H2SetsDAO implements SetsDAO {
 			int setId = rs.getInt("id");
 
 			addMovieToSetPS.setInt(1, setId);
-			addMovieToSetPS.setInt(2, ((DBMovie)m).getId());
+			addMovieToSetPS.setInt(2, ((DBMovie) m).getId());
 			addMovieToSetPS.executeUpdate();
 		} catch (SQLException ex) {
 			ex.printStackTrace();
 		}
 	}
 
+	/** List of change listeners intersted in changes to list of sets */
 	private ArrayList<ChangeListener> setsChangeListeners;
-	
+
+	/**
+	 * Add a sets change listener
+	 * 
+	 * @param changeListener
+	 *            The listener that should be added.
+	 */
 	@Override
 	public void addSetsChangeListener(ChangeListener changeListener) {
-		setsChangeListeners.add(changeListener);		
+		setsChangeListeners.add(changeListener);
 	}
 
+	/**
+	 * Remove the specified changeListener from the list of listeners.
+	 */
 	@Override
 	public void removeSetsChangeListener(ChangeListener changeListener) {
 		setsChangeListeners.remove(changeListener);
 	}
-	
+
+	/**
+	 * Notify all listeners that there has been some change to the list of sets.
+	 */
 	private void fireSetsChangedEvent() {
 		for (ChangeListener l : setsChangeListeners) {
 			l.stateChanged(null);
 		}
 	}
-	
+
 }
